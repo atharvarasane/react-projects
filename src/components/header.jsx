06942a94c69273db7e18a2b28199e909bfa43a8d
atharva rasane <156536832+atharvarasane@users.noreply.github.com>
2025-01-09 21:55:21 +0530

    Add files via upload

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+
+function Header(){
+    return(<nav className="container">
+        <div className="brandlogo ">
+       <img src="brand_logo.png" alt="brand logo" />
+        </div>
+        <ul>
+            <li src="#">Menu</li>
+            <li src="#">location</li>
+            <li src="#">about</li>
+            <li src="#">content</li>
+        </ul>
+        <button>Login</button>
+
+    </nav>)
+}
+export default Header;
---
+++ src/components/mainpage.jsx
@@ -0,0 +1,23 @@
+function Mainpage(){
+    return(<main className="mainpage">
+        <div className="Maintext">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="mainbtn">
+              <button className="Shopnow">Shop Now</button>
+              <button className="Catagary">Catagary</button>
+            </div>
+        </div>
+        <div className="mainopt">
+            <p>Also Available On</p>
+            <div className="brand-icons">
+                <img src="flipkart.png" alt="flipkart"/>
+                <img src="amazon.png" alt="amazon"/>
+            </div>
+        </div>
+        <div className="main-img">
+            <img src="shoe_image.png" alt="shoe image"></img>
+        </div>
+    </main>)
+}
+ export default Mainpage;
